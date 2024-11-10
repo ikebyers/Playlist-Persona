7c020e701dea6c0907756c8383107923c8d6260e
Maher Aboulhosn <maherhosn@gmail.com>
2024-11-10 01:09:00 -0500

    new Current Playlist Updates

--- client/pages/CurrentPlaylistPage.tsx
+++ client/pages/CurrentPlaylistPage.tsx
@@ -1,14 +1,14 @@
 import { ChangeEvent, FormEvent, useEffect, useState } from "react";
-import YouTube from "react-youtube";
+// import YouTube from "react-youtube";
 import auth from "../src/utils/auth";
 import { Song, PlaylistData } from '../src/interfaces/PlaylistData'
 import { createPlaylist } from "../src/api/playlistAPI";
 import { Link } from "react-router-dom";
 
-
 const CurrentPlaylist: React.FC = () => {
   const profile = auth.getProfile();
   const [playlist, setPlaylist] = useState<Song[]>([]);
+  const [expandedCard, setExpandedCard] = useState<number | null>(null);
   const [newPlaylist, setNewPlaylist] = useState<PlaylistData | undefined>({
     id: null,
     title: '',
@@ -16,7 +16,7 @@ const CurrentPlaylist: React.FC = () => {
     assignedUserId: null,
   });
   const [loading, setLoading] = useState(true);
-  const [currentSong, setCurrentSong] = useState<string | null>(null);
+  // const [currentSong, setCurrentSong] = useState<string | null>(null);
   const [loginCheck, setLoginCheck] = useState(false);
   const [songListSaved, setSaved] = useState(false);
 
@@ -56,8 +56,6 @@ const CurrentPlaylist: React.FC = () => {
     }
   }
 
-
-
   useEffect(() => {
     const fetchPlaylist = async () => {
       console.log("before loading");
@@ -76,7 +74,7 @@ const CurrentPlaylist: React.FC = () => {
           body: JSON.stringify({
             questions,
             answers,
-          })
+          }),
         });
 
         if (!response.ok) {
@@ -91,13 +89,11 @@ const CurrentPlaylist: React.FC = () => {
           songTitle: song.songTitle,
           artistName: song.artistName,
           url: song.url,
-          // thumbnailUrl: song.thumbnailUrl || song.snippet?.thumbnails?.default?.url
         }));
+
         console.log(typeof (transformedPlaylist));
         localStorage.setItem("currentPlaylist", JSON.stringify(transformedPlaylist));
-        // transformedPlaylist.forEach((element: Song) => {
-        //   console.log("transformed playlist:", element.songTitle, element.artistName, element.url);
-        // });
+
         setPlaylist(transformedPlaylist);
       } catch (error) {
         console.error("Error fetching playlist:", error);
@@ -109,92 +105,110 @@ const CurrentPlaylist: React.FC = () => {
     fetchPlaylist();
   }, []);
 
-  // const handlePlay = async (index: number, event: Event) => {
-  //   const iframe = document.getElementById(`iframe-${index}`);
-  //   const button = document.getElementById(`button-${index}`);
-  // //   button?.addEventListener();
-  // //   setCurrentSong(videoId);
-  //     iframe.contentWindow.document.getElementById("toggleDivButton").click();
+
+  const toggleCard = (index: number) => {
+    setExpandedCard(expandedCard === index ? null : index);
+  };
+
+  // const playerOptions = {
+  //   height: "0", // Minimize height to simulate audio-only
+  //   width: "0", // Minimize width to simulate audio-only
+  //   playerVars: {
+  //     autoplay: 0,
+  //   },
   // };
 
-  const handleStop = () => {
-    setCurrentSong(null);
-  };
 
-  const playerOptions = {
-    height: "0", // Minimize height to simulate audio-only
-    width: "0", // Minimize width to simulate audio-only
-    playerVars: {
-      autoplay: 0,
-    },
-  };
-
-  return (
+return (
     <div>
-      {loginCheck ? (
-        <div className="w-100 mx-auto p-3 container-fluid center body-text">
-          <div className="card card-margin card-color-alt">
-            <h1 className="body-text-primary">Your Playlist</h1>
-            <p className="body-text-primary">Based on your responses we currated this playlist just for you! Feel free to check out the music and save the playlist to your profile to listen later.</p>
-          </div>
-          {loading ? (
-            <h4>Loading your playlist...</h4>
-          ) : playlist.length > 0 ? (
-            <div className="p-3 center">
-              {!songListSaved ? (
-                <form className='formPlylist' onSubmit={handleSavePlaylist}>
-                  <input
-                    className="form-control container-fluid"
-                    type="text"
-                    name="title"
-                    value={newPlaylist?.title ?? ''}
-                    onChange={handleChange}
-                    placeholder="Name your Plalist to save"
-                  />
-                  <button type="submit" className="rounded-pill btn btn-large">Save Playlist</button>
-                </form>
-              ) : (
-                <div>
-                  <h1>Song List is Saved to profile!</h1>
-                  <Link to="/profile">
-                    <button type="submit" className="rounded-pill btn btn-large">View Playlist</button>
-                  </Link>
-                  <Link to="/home">
-                    <button type="submit" className="rounded-pill btn btn-large">Generate new Playlist</button>
-                  </Link>
+        {loginCheck ? (
+            <div className="w-100 mx-auto p-3 container-fluid center body-text">
+                <div className="card card-margin card-color-alt">
+                    <h1 className="body-text-primary fw-bold">Your Playlist</h1>
+                    <p className="body-text-primary">
+                        Based on your responses, we curated this playlist <span className="fst-italic fw-bold">just for you!</span>
+                    </p>
+                    <p className="body-text-primary">
+                        Feel free to check out the music and save the playlist to your profile to listen later.
+                    </p>
                 </div>
-              )}
-              {playlist.map((song, index) => (
-                <div className="card card-margin card-color" key={index}>
-                  {/* <img src={song.thumbnailUrl} alt={`${song.title} thumbnail`} width="120" height="90" /> */}
-                  {/* <iframe
-              className="w-100 p-3 container-fluid center"
-                width="380"
-                height="360"
-                src={song.url}
-              ></iframe> */}
-                  <iframe id={`iframe-${index}`} className="w-100 p-3 container-fluid center" width="420" height="350" src={`https://www.youtube.com/embed/${song.url}`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-                  <h3>{song.songTitle}</h3>
-                  <h5>Artist: {song.artistName}</h5>
-                  <div className="btn-group">
-                    <button id={`button-${index}`} className="btn-medium" onClick={function () { }}>Play</button>
-                    <button id="pause-video" className="btn-medium" onClick={handleStop}>Pause</button>
-                  </div>
-                  {currentSong === song.url && (
-                    <YouTube videoId={song.url} opts={playerOptions} />
-                  )}
-                </div>
-              ))}
+                {loading ? (
+                    <h4 className="fst-italic">Loading your playlist...</h4>
+                ) : playlist.length > 0 ? (
+                    <div className="p-3 center">
+                        {!songListSaved ? (
+                            <form className='formPlylist' onSubmit={handleSavePlaylist}>
+                                <input
+                                    className="form-control container-fluid"
+                                    type="text"
+                                    name="title"
+                                    value={newPlaylist?.title ?? ''}
+                                    onChange={handleChange}
+                                    placeholder="Name your Plalist to save"
+                                />
+                                <button type="submit" className="rounded-pill btn btn-large">Save Playlist</button>
+                            </form>
+                        ) : (
+                            <div>
+                                <h1>Song List is Saved to profile!</h1>
+                                <Link to="/profile">
+                                    <button type="submit" className="rounded-pill btn btn-large">View Playlist</button>
+                                </Link>
+                                <Link to="/home">
+                                    <button type="submit" className="rounded-pill btn btn-large">Generate new Playlist</button>
+                                </Link>
+                            </div>
+                        )}
+                        {playlist.map((song, index) => (
+                            <div className="card card-margin card-color mx-auto w-75" key={index}>
+                                <div className="card-header d-flex justify-content-between">
+                                    <div className="text-start">
+                                        <h3 className="fw-bold">{song.songTitle}</h3>
+                                        <h5 className="fst-italic">{song.artistName}</h5>
+                                    </div>
+                                    <div className="d-flex">
+                                        <button
+                                            className="btn btn-medium-alt fw-bold ms-auto"
+                                            onClick={() => toggleCard(index)}
+                                        >
+                                            {expandedCard === index ? "⬆" : "⬇"}
+                                        </button>
+                                    </div>
+                                </div>
+                                {expandedCard === index && (
+                                    <div className="card-body">
+                                        <iframe
+                                            id={`iframe-${index}`}
+                                            className="mx-auto p-3 d-flex container-fluid center"
+                                            width="420"
+                                            height="350"
+                                            src={`https://www.youtube.com/embed/${song.url}`}
+                                            title="YouTube video player"
+                                            frameBorder="0"
+                                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                            referrerPolicy="strict-origin-when-cross-origin"
+                                            allowFullScreen
+                                        ></iframe>
+                                        {/* {currentSong === song.url && (
+                                            <YouTube videoId={song.url} opts={playerOptions} />
+                                        )} */}
+                                    </div>
+                                )}
+                            </div>
+                        ))}
+                    </div>
+                ) : (
+                    <div>
+                        <p className="fst-italic">No songs found for your preferences.</p>
+                        <p className="fst-italic">Please try again.</p>
+                    </div>
+                )}
             </div>
-          ) : (
-            <p>No songs found for your preferences. Please try again.</p>
-          )}
-        </div>
-      ) : (
-        <h1> You are not logged in! Please Log In to view this page</h1>
-      )}
+        ) : (
+            <h1> You are not logged in! Please Log In to view this page</h1>
+        )}
     </div>
-  );
+);
 };
 
 export default CurrentPlaylist;
